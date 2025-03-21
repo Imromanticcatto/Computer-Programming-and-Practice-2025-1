b210cf2eaf299421a392398e4eddce372d63be28
Imromanticcatto <mrhyeonjun06@ajou.ac.kr>
2025-03-21 17:44:27 +0900

    답

--- main.c
+++ main.c
@@ -59,7 +59,9 @@ unsigned char uint_to_uchar (unsigned int x);
 
 int main() {
 	/* To Do: 주어진 함수들을 활용해서 16진수 숫자를 입력받고, 화면에 출력해보세요 */
-
+	unsigned int i = input_uint();
+	uint_to_uchar(i);
+	print_uchar(i);
 	
 	return 0;
 }
