fd89f303e7bc988ca94c1333dc30920d0e30eb43
Imromanticcatto <mrhyeonjun06@ajou.ac.kr>
2025-03-21 13:54:43 +0900

    printf 추가

--- main.c
+++ main.c
@@ -5,10 +5,12 @@
  * printf 함수는 문자열을 입력값으로 받아서 화면에 출력합니다. 
  * 문자열을 나타낼 때는 큰 따옴표 안에 문자들을 "문자열"처럼 씁니다.   
  */
+#include <stdio.h>
+#
 
 /* To Do: include 지시어로 stdio.h 라이브러리 헤더 파일을 포함시켜보세요 */
 
 int main() {
 	/* To Do: 리턴값을 printf 함수의 호출식으로 바꿔보세요, printf를 호출할 때 인자는 "Hello, world!"로 넣어보세요 */
-	return 0;
+	return printf("Hello, world!");
 }
